f37d9517ea8d14de2a85e6687cece24ee4d2c776
Lennard B <104066505+ooxiel@users.noreply.github.com>
2022-06-24 17:35:40 +0200

    Add files via upload

---
+++ PersonalverwaltungAMGIS/src/main/java/com/personalverwaltung/PersonalverwaltungAMGIS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.personalverwaltung;
+
+/**
+ *
+ * @author Franz
+ */
+public class PersonalverwaltungAMGIS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
