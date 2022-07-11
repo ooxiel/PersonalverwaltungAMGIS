91e8b848354a05fe4fe3aae2be5964d463c38965
ooxiel <becker.lennard@outlook.de>
2022-07-11 20:02:13 +0200

    Version 1.1

---
+++ src/main/java/GUI/HR_erstellen.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import javax.swing.*;
+
+public class HR_erstellen extends JFrame {
+
+    private JTextField textField1;
+    private JTextField textField2;
+
+    public static void main(String[] args) {
+        new HR_erstellen();
+    }
+
+    public HR_erstellen(){
+
+    }
+}
