d705037a7d8cd65a72d1a8c833948c394af7cc50
farnz8 <franz.weyh@hotmail.com>
2022-07-12 18:09:52 +0200

    quick optimization

--- src/main/java/com/AMGIS/Data_Handling/AccountErzeugen.java
+++ src/main/java/com/AMGIS/Data_Handling/AccountErzeugen.java
@@ -5,13 +5,11 @@ public class AccountErzeugen {
 
     }
     protected static String kontoname_erzeugen(int id, String vorname, String nachname){
-        String kontoname=nachname.substring(0,1).toLowerCase()+"_"+vorname.toLowerCase()+id+"0";
-        return kontoname;
+        return nachname.substring(0,1).toLowerCase()+"_"+vorname.toLowerCase()+id+"0";
     }
     protected static String passwort_erzeugen(int id, String vorname, String nachname){
         String[] x = {"!", "?", "/","=",">","-","<","$","&"};
-        String passwort=nachname.substring(1,3)+x[(int)(Math.random() * 9)]+"_"+id*420/69+x[(int)(Math.random() * 9)]+vorname.substring(0,1).toUpperCase()+x[(int)(Math.random() * 9)];
-        return passwort;
+        return nachname.substring(1,3)+x[(int)(Math.random() * 9)]+"_"+id*420/69+x[(int)(Math.random() * 9)]+vorname.substring(0,1).toUpperCase()+x[(int)(Math.random() * 9)];
     }
 
 }
