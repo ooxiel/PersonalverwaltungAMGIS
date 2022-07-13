0500d2b4864af3583e6d23d0a66eb673d3d7ccea
ooxiel <becker.lennard@outlook.de>
2022-07-13 22:32:04 +0200

    1.3

--- src/main/java/com/AMGIS/Facade/LoginFacade.java
+++ src/main/java/com/AMGIS/Facade/LoginFacade.java
@@ -15,26 +15,17 @@ public class LoginFacade {
 
         LoginProof loginToProof = new LoginProof();
 
-        if(!loginToProof.checkNull(username, password)){
+        if (!loginToProof.checkNull(username, password)) {
             return false;
 
-            }else if (!loginToProof.username(username)){
-                return false;
+        } else if (!loginToProof.username(username)) {
+            return false;
 
-            }else if (!loginToProof.passwordProof(password)){
-                    return false;
+        } else if (!loginToProof.passwordProof(password)) {
+            return false;
 
-            }
+        }
 
         return true;
-    }
-    public String newUsernamePassword (Mitarbeiter mitarbeiter){
-
-        LoginGenerate generate = new LoginGenerate();
-
-            String newUsername = generate.generateUsername(mitarbeiter);
-            String newPassword = generate.passwordGenerate();
-
-        return "Username: "+newUsername+" | Password: "+newPassword;
     }
 }
